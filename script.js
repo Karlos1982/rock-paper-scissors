d8eb87f2ec835a9b3b577659cc5e9733b595ce42
Karlos <kands.2004@hotmail.com>
2023-02-22 11:56:41 +1000

    updated ui

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ play another round
 announce winner of 5 rounds
 */
 
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+const roundResult = document.querySelector('#round-result');
+const computerPoints = document.querySelector('#computer-score');
+const playerPoints = document.querySelector('#player-score');
+const gameResult = document.querySelector('#game-result');
+
 //declare variables
 const choices = ['rock', 'paper', 'scissors'];
 let playerSelection;
@@ -13,55 +21,76 @@ let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
+//set random computer choice
+function getComputerChoice(){
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function clickButton(event) {
+
+    const playerSelection = event.target.id;
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+rockButton.addEventListener('click', clickButton);
+paperButton.addEventListener('click', clickButton);
+scissorsButton.addEventListener('click', clickButton);
+
+function reset() {
+    
+    playerScore = 0;
+    playerPoints.textContent = "Player :";
+    computerScore = 0;
+    computerPoints.textContent = "Computer :";
+    roundResult.textContent = "";
+    
+}
+
 
 //play a round
 function playRound(playerSelection, computerSelection){
-    //set random computer choice
-    function getComputerChoice(){
-        return choices[Math.floor(Math.random()*choices.length)];
-    }
-    computerSelection = getComputerChoice();
     
-    //get user choice
-    playerSelection = 
-    prompt("Choose Rock, Paper or Scissors").toLowerCase();
+    function playerWin() {
+        playerScore++;
+        playerPoints.textContent = `Player : ${playerScore}`;
+    }
+    
+    function computerWin() {
+        computerScore++;
+        computerPoints.textContent = `Computer : ${computerScore}`;
+    }
 
     //determine winner
     if (playerSelection === computerSelection)
-    console.log('It is a draw');
+    roundResult.textContent = 'It is a draw';
     else if (playerSelection === 'rock' && computerSelection === 'paper')
-    console.log('You Lose! Paper beats Rock');
+    roundResult.textContent = 'You Lose! Paper beats Rock';
     else if (playerSelection === 'rock' && computerSelection === 'scissors')
-    console.log('You Win! Rock beats Scissors');
+    roundResult.textContent = 'You Win! Rock beats Scissors';
     else if (playerSelection === 'paper' && computerSelection === 'rock')
-    console.log('You Win! Paper beats Rock');
+    roundResult.textContent = 'You Win! Paper beats Rock';
     else if (playerSelection === 'paper' && computerSelection === 'scissors')
-    console.log('You Lose! Scissors beat Paper');
+    roundResult.textContent = 'You Lose! Scissors beat Paper';
     else if (playerSelection === 'scissors' && computerSelection === 'rock')
-    console.log('You Lose! Rock beats Scissors');
+    roundResult.textContent = 'You Lose! Rock beats Scissors';
     else if (playerSelection === 'scissors' && computerSelection === 'paper')
-    console.log('You Win! Scissors beat Paper');
+    roundResult.textContent = 'You Win! Scissors beat Paper';
     
     //Scoring
         if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper')
-        playerScore++;
+        playerWin();
         else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock')
-        computerScore++;
+        computerWin();
+
+        // if playerScore === 5 => display You Win => reset scores
+        if (playerScore === 5){
+            gameResult.textContent = 'Congratulations, You Won!!!';
+            reset();
+        } 
+        // if computerScore === 5 => display You Lose => reset scores
+        else if (computerScore === 5) {
+            gameResult.textContent = 'Sorry but you lost :(';
+            reset();
+        } 
 }
-
-
-//play a game of 5 rounds
-function game(){
-    for (let i = 0; i < 5; i++){
-        playRound();
-        console.log(playerScore +' : ' +computerScore)
-    }
-        if (playerScore > computerScore)
-        console.log('You won the game!!!')
-        else if (computerScore > playerScore)
-        console.log('You lost the game!!!')
-        else console.log('It was a draw')
-}
-
-
-game();
